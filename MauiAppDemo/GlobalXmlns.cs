facec94ec59232fd77d45b676277d35c220cf8d6
ayanik <ayanik@uni-yaz.com>
2025-09-28 17:31:18 +0300

    Add project files.

---
+++ MauiAppDemo/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiAppDemo")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiAppDemo.Pages")]
